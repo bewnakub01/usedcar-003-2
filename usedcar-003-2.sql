7359225d16c16ec8fe2ad6c7f973ee45b3afd9e7
bewnakub01 <napathbew@gmail.com>
2019-12-20 15:31:07 +0700

    add

--- usedcar-003-2.sql
+++ usedcar-003-2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 19, 2019 at 02:12 AM
+-- Generation Time: Dec 20, 2019 at 12:30 AM
 -- Server version: 8.0.17
--- PHP Version: 7.3.8
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `usedcar-003`
+-- Database: `usedcar-003-2`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `car` (
   `id` int(11) NOT NULL,
-  `carType` int(11) NOT NULL,
   `brand` varchar(50) NOT NULL,
   `model` varchar(80) NOT NULL,
   `color` varchar(20) NOT NULL,
@@ -47,14 +46,16 @@ CREATE TABLE `car` (
 -- Dumping data for table `car`
 --
 
-INSERT INTO `car` (`id`, `carType`, `brand`, `model`, `color`, `license`, `province`, `modelYear`, `price`, `postedBy`, `postedDate`, `carpic`) VALUES
-(2, 1, 'NISAN', 'NISAN0052', 'คำ', 'นห-9158', 'นครปฐม', '2017', 1500000, 1, '2019-12-05 00:00:00', '1 (1).jpg'),
-(3, 1, 'BMWTI', 'BMW25', 'ขาว', 'ยห-2851', 'ราชบุรี', '2019', 2980000, 1, '2019-12-03 00:00:00', '1 (2).jpg'),
-(4, 1, 'NISAN', 'NISAN01', 'ฟ้า', 'กท-2846', 'ราชบุรี', '2018', 1150000, 10, '2019-12-14 00:00:00', '1 (3).jpg'),
-(5, 2, 'VIGO', 'VIGOZ4', 'ขาว', 'กท-103', 'ราชบุรี', '2018', 1580000, 1, '2019-12-05 00:00:00', '2 (1).jpg'),
-(6, 3, 'TOYOTA', '2017', 'ขาว', 'กท-845', 'นครปฐม', '2018', 1110000, 10, '2019-12-05 00:00:00', '3 (2).jpg'),
-(7, 2, 'VIGO', 'VIGOGG', 'เทา', 'กท-2846', 'ราชบุรี', '2018', 580000, 10, '2019-12-05 00:00:00', '2 (2).jpg'),
-(8, 2, 'VIGO', 'VIGORG', 'ดำ', 'กท-845', 'ราชบุรี', '2018', 280000, 10, '2019-12-05 00:00:00', '2 (3).jpg');
+INSERT INTO `car` (`id`, `brand`, `model`, `color`, `license`, `province`, `modelYear`, `price`, `postedBy`, `postedDate`, `carpic`) VALUES
+(2, 'NISAN', 'NISAN0052', 'คำ', 'นห-9158', 'นครปฐม', '2017', 1500000, 1, '2019-12-05 00:00:00', '1 (1).jpg'),
+(3, 'BMWTI', 'BMW25', 'ขาว', 'ยห-2851', 'ราชบุรี', '2019', 2980000, 1, '2019-12-03 00:00:00', '1 (2).jpg'),
+(4, 'NISAN', 'NISAN01', 'ฟ้า', 'กท-2846', 'ราชบุรี', '2018', 1150000, 10, '2019-12-14 00:00:00', '1 (3).jpg'),
+(5, 'VIGO', 'VIGOZ4', 'ขาว', 'กท-103', 'ราชบุรี', '2018', 1580000, 1, '2019-12-05 00:00:00', '2 (1).jpg'),
+(6, 'TOYOTA', '2017', 'ขาว', 'กท-845', 'นครปฐม', '2018', 1110000, 10, '2019-12-05 00:00:00', '3 (2).jpg'),
+(7, 'VIGO', 'VIGOGG', 'เทา', 'กท-2846', 'ราชบุรี', '2018', 580000, 10, '2019-12-05 00:00:00', '2 (2).jpg'),
+(8, 'VIGO', 'VIGORG', 'ดำ', 'กท-845', 'ราชบุรี', '2018', 280000, 10, '2019-12-05 00:00:00', '2 (3).jpg'),
+(11, 'HONDA', 'HONDA-R1', 'เทา', 'ม-44', 'ราชบุรี', '2019', 198000, 3, '2019-12-20 14:48:49', '4 (1).jpg'),
+(12, 'BMW', 'BMW-Z4', 'ขาว', 'ม-112', 'กรุงเทไ', '2018', 1921111, 3, '2019-12-20 14:51:03', '4 (2).jpg');
 
 -- --------------------------------------------------------
 
@@ -104,7 +105,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `car`
 --
 ALTER TABLE `car`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `customers`
